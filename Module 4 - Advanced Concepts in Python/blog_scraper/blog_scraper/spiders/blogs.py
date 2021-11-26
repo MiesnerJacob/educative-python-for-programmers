413ba0ab30dc44e6347c1354620e1fc6a02fbcd5
miesnerjacob <jacob@sightly.com>
2021-11-26 16:31:49 -0500

    added web scraping chapter coursework

---
+++ Module 4 - Advanced Concepts in Python/blog_scraper/blog_scraper/spiders/blogs.py
@@ -0,0 +1,21 @@
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+from ..items import BlogScraperItem
+
+
+class MyBlogSpider(Spider):
+    name = 'mouse'
+    start_urls = ['http://blog.pythonlibrary.org']
+
+    def parse(self, response):
+        selector = Selector(response)
+        blog_titles = selector.xpath("//h1[@class='entry-title']")
+        selections = []
+
+        for data in blog_titles:
+            selection = BlogScraperItem()
+            selection['title'] = data.xpath("a/text()").extract()
+            selection['link'] = data.xpath("a/@href").extract()
+            selections.append(selection)
+
+        return selections
